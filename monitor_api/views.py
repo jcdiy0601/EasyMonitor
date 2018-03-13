18d4dc63a98657d5ff3f06970b1c1b4d1864d4fe
jcdiy0601 <15210942688@163.com>
2018-03-13 14:45:53 +0800

    2018031303

--- monitor_api/views.py
+++ monitor_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from utils.monitor_api_auth import monitor_api_auth
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+@monitor_api_auth
+def client_configs(request):
+    """客户端向api提交申请并返回数据"""
+    pass
