1ff6013b05d9cc985c3fa1ea895979955bb48537
jcdiy0601 <15210942688@163.com>
2018-07-13 13:40:42 +0800

    20180713

--- utils/management.py
+++ utils/management.py
@@ -44,29 +44,29 @@ class ManagementUtility(object):
         """开始监听触发器"""
         print('启动监听触发器服务...')
         time.sleep(1)
-        # pid = os.fork()
-        # # 父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端
-        # if pid > 0:
-        #     sys.exit(0)  # 父进程退出
-        # # 从母体环境脱离
-        # os.chdir('/')  # chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统，也可以改变到对于守护程序运行重要的文件所在目录
-        # os.umask(0)  # 调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask
-        # os.setsid()  # setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离
-        # # 执行第二次fork
-        # pid = os.fork()
-        # if pid > 0:
-        #     sys.exit(0)  # 第二个父进程退出
-        # # 进程已经是守护进程了，重定向标准文件描述符
-        # sys.stdout.flush()
-        # sys.stderr.flush()
-        # # dup2函数原子化地关闭和复制文件描述符，重定向到/dev/nul，即丢弃所有输入输出
-        # with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
-        #     os.dup2(read_null.fileno(), sys.stdin.fileno())
-        #     os.dup2(write_null.fileno(), sys.stdout.fileno())
-        #     os.dup2(write_null.fileno(), sys.stderr.fileno())
-        # # 写入pid文件
-        # with open(self.trigger_watch_pid, 'w') as f:
-        #     f.write(str(os.getpid()))
+        pid = os.fork()
+        # 父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端
+        if pid > 0:
+            sys.exit(0)  # 父进程退出
+        # 从母体环境脱离
+        os.chdir('/')  # chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统，也可以改变到对于守护程序运行重要的文件所在目录
+        os.umask(0)  # 调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask
+        os.setsid()  # setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离
+        # 执行第二次fork
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)  # 第二个父进程退出
+        # 进程已经是守护进程了，重定向标准文件描述符
+        sys.stdout.flush()
+        sys.stderr.flush()
+        # dup2函数原子化地关闭和复制文件描述符，重定向到/dev/nul，即丢弃所有输入输出
+        with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
+            os.dup2(read_null.fileno(), sys.stdin.fileno())
+            os.dup2(write_null.fileno(), sys.stdout.fileno())
+            os.dup2(write_null.fileno(), sys.stderr.fileno())
+        # 写入pid文件
+        with open(self.trigger_watch_pid, 'w') as f:
+            f.write(str(os.getpid()))
         trigger_handle_obj = TriggerHandle()
         trigger_handle_obj.start_watching()
 
@@ -99,29 +99,29 @@ class ManagementUtility(object):
         """启动主机存活检测服务"""
         print('启动主机存活检测服务...')
         time.sleep(1)
-        # pid = os.fork()
-        # # 父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端
-        # if pid > 0:
-        #     sys.exit(0)  # 父进程退出
-        # # 从母体环境脱离
-        # os.chdir('/')  # chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统，也可以改变到对于守护程序运行重要的文件所在目录
-        # os.umask(0)  # 调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask
-        # os.setsid()  # setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离
-        # # 执行第二次fork
-        # pid = os.fork()
-        # if pid > 0:
-        #     sys.exit(0)  # 第二个父进程退出
-        # # 进程已经是守护进程了，重定向标准文件描述符
-        # sys.stdout.flush()
-        # sys.stderr.flush()
-        # # dup2函数原子化地关闭和复制文件描述符，重定向到/dev/nul，即丢弃所有输入输出
-        # with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
-        #     os.dup2(read_null.fileno(), sys.stdin.fileno())
-        #     os.dup2(write_null.fileno(), sys.stdout.fileno())
-        #     os.dup2(write_null.fileno(), sys.stderr.fileno())
-        # # 写入pid文件
-        # with open(self.host_alive_pid, 'w') as f:
-        #     f.write(str(os.getpid()))
+        pid = os.fork()
+        # 父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端
+        if pid > 0:
+            sys.exit(0)  # 父进程退出
+        # 从母体环境脱离
+        os.chdir('/')  # chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统，也可以改变到对于守护程序运行重要的文件所在目录
+        os.umask(0)  # 调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask
+        os.setsid()  # setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离
+        # 执行第二次fork
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)  # 第二个父进程退出
+        # 进程已经是守护进程了，重定向标准文件描述符
+        sys.stdout.flush()
+        sys.stderr.flush()
+        # dup2函数原子化地关闭和复制文件描述符，重定向到/dev/nul，即丢弃所有输入输出
+        with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
+            os.dup2(read_null.fileno(), sys.stdin.fileno())
+            os.dup2(write_null.fileno(), sys.stdout.fileno())
+            os.dup2(write_null.fileno(), sys.stderr.fileno())
+        # 写入pid文件
+        with open(self.host_alive_pid, 'w') as f:
+            f.write(str(os.getpid()))
         data_handle_obj = DataHandle()
         data_handle_obj.looping()
 
